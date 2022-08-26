00d93a422f3b838e411a6568bff8f5dea1e9ece6
= <=>
2022-08-26 14:53:41 +0700

    add filter history overbooking

--- app/Http/Controllers/Web/history_overbooking/DbController.php
+++ app/Http/Controllers/Web/history_overbooking/DbController.php
@@ -9,8 +9,31 @@ use Vanguard\Http\Controllers\Web\history_overbooking\DbController as LogBankDB;
 class DbController extends Controller 
 {
     /** GET DATA */
-    public static function getAll(){
-        $data = DB::SELECT("SELECT * FROM vw_Overbooking_H");
+    public static function getAll($filter = ''){
+        $filter = rtrim(base64_decode($filter));
+        $where = $filter != '' ? "WHERE $filter" : '';
+        $data = DB::SELECT("SELECT * FROM vw_Overbooking_H $where");
+
+        return $data;
+    }
+
+    public static function getColumnName($excludeColumn = []){
+        $whereClause = '';
+        if(count($excludeColumn) > 0){
+            $whereClause = 'AND column_id NOT IN (';
+            for ($i=0; $i < count($excludeColumn); $i++) { 
+                $whereClause .= "'$excludeColumn[$i]',";
+            }
+            $whereClause = rtrim($whereClause, ',');
+            $whereClause .= ')';
+        }
+        $data = DB::SELECT("SELECT * FROM SchemaInfo where table_name = 'vw_overbooking_h' $whereClause");
+
+        return $data;
+    }
+
+    public static function getColumnData($col){
+        $data = DB::SELECT("SELECT DISTINCT $col FROM VW_OVERBOOKING_H r GROUP BY $col");
 
         return $data;
     }
---
+++ resources/views/history_overbooking/component/tableData.blade.php
@@ -0,0 +1,28 @@
+<div class="card-body">
+    <div class="table-responsive">
+        <table class="table t-overbooking" style="width: 100%">
+            <thead>
+                <tr>    
+                    {{-- <th>Action</th> --}}
+                    <th>Status</th>
+                    <th>Partner ID</th>
+                    <th>Sender Bank</th>
+                    <th>Sender Account</th>
+                    <th>Sender Ammount</th>
+                    <th>Notes</th>
+                    <th>Recipient Bank Name</th>
+                    <th>Recipient Account</th>
+                    <th>Recipient Ammount</th>
+                    <th>Amount</th>
+                    <th>Execution Time</th>
+                    <th>Description</th>
+                    <th>Internal Status</th>
+                    <th>Status Message</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</div>
