b345b047b8c21431fbce831379d81206986e26a3
Danetra <danetra05@gmail.com>
2022-10-07 10:00:18 +0700

    1

--- app/Http/Controllers/Web/Users/UsersController.php
+++ app/Http/Controllers/Web/Users/UsersController.php
@@ -82,22 +82,7 @@ class UsersController extends Controller
         $id = $request->tipeID;
 
         $option = "<option value='0'>Select a Province</option>";
-        // if($id === '4')
-        // {
-        //     $provinces = Province::all()->where('role_id', $id);
-        //     foreach ($provinces as $province)
-        //     {
-        //         $option .= '<option value="'.$province->prop_id.'">'.$province->prop_nama.'</option>';
-        //     }
-        // }
-        // else
-        // {
-        //     $provinces = Province::all()->where('role_id', '4');
-        //     foreach ($provinces as $province)
-        //     {
-        //         $option .= '<option value="'.$province->prop_id.'">'.$province->prop_nama.'</option>';
-        //     }
-        // }
+
         $provinces = Province::all();
         foreach ($provinces as $province)
         {
