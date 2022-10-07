5517f19033b649b8c86e33a2bdaa650040d6e2da
Danetra <danetra05@gmail.com>
2022-10-07 15:34:29 +0700

    1

--- resources/views/user/partials/row.blade.php
+++ resources/views/user/partials/row.blade.php
@@ -52,8 +52,8 @@
         </a>
 
         <a href="{{ route('users.destroy', $user) }}" class="btn btn-icon" title="@lang('Delete User')"
-            data-toggle="tooltip" data-placement="top" data-method="DELETE" data-confirm-title="@lang('Please Confirm')"
-            data-confirm-text="@lang('Are you sure that you want to delete this user?')" data-confirm-delete="@lang('Yes, delete him!')">
+            data-method="DELETE" data-confirm-title="@lang('Please Confirm')" data-confirm-text="@lang('Are you sure that you want to delete this user?')"
+            data-confirm-delete="@lang('Yes, delete him!')">
             <i class="fas fa-trash"></i>
         </a>
     </td>
