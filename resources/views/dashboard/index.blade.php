dfaa9b0e429b0a45b96a06dc89a5536b6cda23fd
= <=>
2022-10-18 08:28:19 +0700

    test

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -255,7 +255,7 @@
                     <div class="card-body" style="height: 500px">
                         <div class="embed-responsive embed-responsive-21by9">
                             <iframe class="embed-responsive-item"
-                                src="http://103.245.225.162:3000/monitoring"></iframe>
+                                src="https://middlewareapi.guitekno.com/monitoring"></iframe>
                         </div>
                         {{-- <div class="embed-responsive embed-responsive-21by9">
                             <iframe class="embed-responsive-item"
