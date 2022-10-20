a218d4d151752f7abc3d7d10d4d06c7002ae4537
Danetra <danetra05@gmail.com>
2022-10-20 16:56:54 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,7 @@ Route::get('auth/{provider}/callback', 'Auth\SocialAuthController@handleProvider
 //Overbooking
 Route::get('overbooking', 'Overbooking\OverbookingController@index');
 Route::get('overbooking-pdf/{filter}', 'PDF\PdfOverbookingController@generatePDF');
+Route::get('/overbooking-pdf-test', 'PDF\PdfOverbookingController@test');
 
 /**
  * Impersonate Routes
